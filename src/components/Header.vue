824b42d7bc8ad9be1c2b1e6ec090e8601267cb33
alexanderiksson <Alexerik69@gmail.com>
2025-01-29 13:42:34 +0100

    Added tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <header class="py-6">
+        <div class="content flex">
+            <div>sadas</div>
+            <nav class="ml-auto">
+                <ul class="flex gap-12">
+                    <li>
+                        <RouterLink to="/">Market</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/">About</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/">Contact</RouterLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
