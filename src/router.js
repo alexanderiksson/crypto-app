80e10632e07601f3a7c62e468cf21470eba85d8e
alexanderiksson <Alexerik69@gmail.com>
2025-01-29 13:44:38 +0100

    Formated code

--- src/router.js
+++ src/router.js
@@ -1,11 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import Home from './views/Home.vue'
-
+import Home from "./views/Home.vue";
 
 export default createRouter({
     history: createWebHistory(),
-    routes: [
-        {component: Home, path: '/'}
-    ]
-})
+    routes: [{ component: Home, path: "/" }],
+});
