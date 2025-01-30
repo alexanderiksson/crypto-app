0949f2b1a41bef307cda7de5106304345fabc110
alexanderiksson <Alexerik69@gmail.com>
2025-01-30 15:36:40 +0100

    Added routes and views

--- src/router.js
+++ src/router.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Contact from "./views/Contact.vue";
 
 export default createRouter({
     history: createWebHistory(),
-    routes: [{ component: Home, path: "/" }],
+    routes: [
+        { component: Home, path: "/" },
+        { component: About, path: "/about" },
+        { component: Contact, path: "/contact" },
+    ],
 });
---
+++ src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="content">
+        <h1 class="text-4xl mb-8">Contact us</h1>
+    </div>
+</template>
