a2874108ad0fecc0def1c85bcdbdf8bb4d9c724e
alexanderiksson <Alexerik69@gmail.com>
2025-02-04 11:29:25 +0100

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: ['eslint:recommended', 'plugin:vue/vue3-essential'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+};
