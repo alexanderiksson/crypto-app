27978d05aa3b16cfcf51ab09d4e83554b1dc9ade
alexanderiksson <Alexerik69@gmail.com>
2025-02-04 14:25:56 +0100

    Added fetching inside chart component

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,15 +1,57 @@
 <script setup>
-import { LineChart } from "vue-chart-3";
-import { Chart, registerables } from "chart.js";
+    import { LineChart } from 'vue-chart-3';
+    import { Chart, registerables } from 'chart.js';
+    import Loader from './Loader.vue';
+    import { useFetch } from '../composables/useFetch';
+    import { computed, ref } from 'vue';
 
-Chart.register(...registerables);
+    Chart.register(...registerables);
 
-const props = defineProps({
-    priceHistory: Object,
-    chartOptions: Object,
-});
+    const props = defineProps({
+        crypto: String
+    });
+
+    const { data, error, loading } = useFetch(
+        `https://api.coincap.io/v2/assets/${props.crypto}/history?interval=m1`
+    );
+
+    const price = computed(() =>
+        data.value && data.value.data ? data.value.data : []
+    );
+
+    const chartData = computed(() => ({
+        labels: price.value.map((item) =>
+            new Date(item.time).toLocaleTimeString()
+        ),
+        datasets: [
+            {
+                label: 'Price (USD)',
+                data: price.value.map((item) => item.priceUsd),
+                borderColor: '#592dfa',
+                tension: 0.4,
+                borderWidth: 2,
+                pointRadius: 0
+            }
+        ]
+    }));
+
+    const chartOptions = ref({
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+            duration: 0
+        }
+    });
 </script>
 
 <template>
-    <LineChart :chartData="chartData" :options="chartOptions" />
+    <template v-if="loading">
+        <Loader />
+    </template>
+
+    <p v-else-if="error">Something went wrong...</p>
+
+    <template v-else>
+        <LineChart :chartData="chartData" :options="chartOptions" />
+    </template>
 </template>
