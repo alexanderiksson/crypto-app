f889eddcacd4b05f96a0fa59133735bb63499ad6
alexanderiksson <Alexerik69@gmail.com>
2025-02-17 11:30:05 +0100

    Added content to pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="content">
         <h1 class="text-4xl font-semibold mb-8">About us</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+            expedita voluptatibus placeat ratione quos voluptates atque,
+            blanditiis quasi voluptate in repellat dignissimos voluptas quas
+            maxime iure quaerat dolores assumenda? Sunt natus quae excepturi
+            aliquid, suscipit exercitationem, quidem itaque iure corporis vero
+            sed vel tempora ab et recusandae, debitis sit dolor?
+        </p>
     </div>
 </template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="content">
         <h1 class="text-4xl font-semibold mb-8">Contact us</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed magni
+            laudantium mollitia recusandae saepe, necessitatibus eum unde
+            repellat! Dicta, fuga.
+        </p>
     </div>
 </template>
