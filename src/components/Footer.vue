b716f1c25e042e26620bfaeba425691537c4e18d
alexanderiksson <Alexerik69@gmail.com>
2025-02-17 13:38:49 +0100

    Added nav to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,24 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-    <footer class="bg-secondaryBackground py-16 mt-24 text-center">
+    <footer class="bg-secondaryBackground py-2 mt-24 text-center">
         <div class="content">
-            <p>Copyright &copy; 2025</p>
+            <nav class="my-16">
+                <ul class="flex justify-center gap-10">
+                    <li>
+                        <RouterLink to="/">Market</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/about">About</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/contact">Contact</RouterLink>
+                    </li>
+                </ul>
+            </nav>
+            <p class="text-xs text-neutral-400">Copyright &copy; 2025</p>
         </div>
     </footer>
 </template>
