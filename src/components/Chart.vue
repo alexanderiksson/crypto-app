55c958c12126be75f8d87627ae3eb6bcf50113a8
alexanderiksson <Alexerik69@gmail.com>
2025-02-20 16:10:42 +0100

    Styling

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -37,7 +37,7 @@
 
     const chartOptions = ref({
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
         animation: {
             duration: 0
         }
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -11,46 +11,52 @@
 </script>
 
 <template>
-    <table class="w-full">
-        <thead>
-            <tr>
-                <th class="text-left text-xl py-4 px-2">Name</th>
-                <th class="text-left text-xl py-4 px-2">Price</th>
-                <th class="text-left text-xl py-4 px-2">Change &percnt;</th>
-                <th class="hidden md:table-cell text-left text-xl py-4 px-2">
-                    Volume &#40;24h&#41;
-                </th>
-                <th class="hidden md:table-cell text-left text-xl py-4 px-2">
-                    Market Cap
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="crypto in data" :key="crypto.id">
-                <td class="py-4 px-2">
-                    <RouterLink :to="`/crypto/${crypto.id}`">{{
-                        crypto.name
-                    }}</RouterLink>
-                </td>
-                <td class="py-4 px-2">
-                    {{ formatPrice(crypto.priceUsd) }}
-                </td>
-                <td
-                    :class="
-                        crypto.changePercent24Hr > 0
-                            ? 'text-green-500'
-                            : 'text-red-500' + ' py-4 px-2'
-                    "
-                >
-                    {{ formatPercent(crypto.changePercent24Hr) }}
-                </td>
-                <td class="hidden md:table-cell py-4 px-2">
-                    {{ formatPrice(crypto.volumeUsd24Hr, 'compact') }}
-                </td>
-                <td class="hidden md:table-cell py-4 px-2">
-                    {{ formatPrice(crypto.marketCapUsd, 'compact') }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="overflow-x-auto">
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th class="text-left text-base py-4 px-2">Name</th>
+                    <th class="text-left text-base py-4 px-2">Price</th>
+                    <th class="text-left text-base py-4 px-2">Change</th>
+                    <th
+                        class="hidden md:table-cell text-left text-base py-4 px-2"
+                    >
+                        Volume &#40;24h&#41;
+                    </th>
+                    <th
+                        class="hidden md:table-cell text-left text-base py-4 px-2"
+                    >
+                        Market Cap
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-secondaryBackground">
+                <tr v-for="crypto in data" :key="crypto.id">
+                    <td class="py-4 px-2">
+                        <RouterLink :to="`/crypto/${crypto.id}`">{{
+                            crypto.name
+                        }}</RouterLink>
+                    </td>
+                    <td class="py-4 px-2">
+                        {{ formatPrice(crypto.priceUsd) }}
+                    </td>
+                    <td
+                        :class="
+                            crypto.changePercent24Hr > 0
+                                ? 'text-green-500'
+                                : 'text-red-500' + ' py-4 px-2'
+                        "
+                    >
+                        {{ formatPercent(crypto.changePercent24Hr) }}
+                    </td>
+                    <td class="hidden md:table-cell py-4 px-2">
+                        {{ formatPrice(crypto.volumeUsd24Hr, 'compact') }}
+                    </td>
+                    <td class="hidden md:table-cell py-4 px-2">
+                        {{ formatPrice(crypto.marketCapUsd, 'compact') }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
